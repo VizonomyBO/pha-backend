1ebe71c0ca4d18771c0e97518d561d139a1a0f30
Rolando Troche <rtrochevenegas@gmail.com>
2022-04-01 15:22:13 -0400

    added cors

--- src/app.ts
+++ src/app.ts
@@ -11,14 +11,18 @@ import cartoRouter from './routes/cartodb.route';
 import authRouter from './routes/auth.route';
 import * as swaggerUi from 'swagger-ui-express';
 import * as swaggerDocument from './swagger.json';
+import * as cors from 'cors';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cors());
 
 app.get('/', async (req: Request, res: Response) => {
   res.json({ success: true, message: 'Express working' });
 });
+
+app.options('*', cors());
 
 const port = 9000;
 app.listen(port, () => {
