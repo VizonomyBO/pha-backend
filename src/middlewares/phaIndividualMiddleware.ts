7352e9abb269c2fc20df57094da7051afa993b07
Lua <xdaddisxd@gmail.com>
2022-04-21 13:28:06 -0400

    delete the stupid validator

--- src/middlewares/phaIndividualMiddleware.ts
+++ src/middlewares/phaIndividualMiddleware.ts
@@ -2,13 +2,13 @@ import { validationErrorToString } from '../utils';
 import { NextFunction, Request, Response } from 'express';
 import { PhaIndividual } from '../@types/database';
 import BadRequestError from '../errors/BadRequestError';
-import validatePhaIndividual from '../validation/PhaIndividual';
+// import validatePhaIndividual from '../validation/PhaIndividual';
 
 export const phaIndividualMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const phaIndividual = req.body as PhaIndividual;
-  if (validatePhaIndividual(phaIndividual)) {
+  if (phaIndividual) {
     next();
     return;
   }
-  next(new BadRequestError(validationErrorToString(validatePhaIndividual)));
+  // next(new BadRequestError(validationErrorToString({ errors: null, schema: 'asdf', schemaEnv: 'asdf' })));
 };
