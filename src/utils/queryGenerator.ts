4bd85ee78ddff1f3cff725b9714e4b76a456896f
Lua <xdaddisxd@gmail.com>
2022-05-31 15:01:33 -0400

    fix problems with query

--- src/utils/queryGenerator.ts
+++ src/utils/queryGenerator.ts
@@ -712,7 +712,7 @@ export const  automaicallySetSuperstarBadgeQuery = () => {
         AND visible_percentage < 0.5
         AND local_percentage < 0.5
       )
-    ) OR retailer_id NOT IN (SELECT retailer_id FROM ${PHA_INDIVIDUAL}) WHERE submission_status='Approved') AND retailer_id NOT IN (
+    ) OR retailer_id NOT IN (SELECT retailer_id FROM ${PHA_INDIVIDUAL} WHERE submission_status='Approved')) AND retailer_id NOT IN (
       SELECT retailer_id FROM ${SUPERSTART_LAST_VALUE_TABLE}
        WHERE last_value IS False);
     INSERT INTO ${SUPERSTAR_UPDATES_TABLE}
